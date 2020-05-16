c245b320a8041cad811d79806e911f4629a92799
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-05-16 15:15:25 +0100

    Can access data from API

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
-<template>
+<template lang="html">
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Anime Studio</h1>
+    <film-list :films="films"></film-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FilmList from "@/components/FilmList";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    "film-list": FilmList,
+  },
+  data() {
+    return {
+      films: [],
+    };
+  },
+  methods: {
+    getFilms: function() {
+      fetch("https://ghibliapi.herokuapp.com/films")
+      .then(res => res.json())
+      .then(filmData => {
+        this.films = filmData;
+      })
+    }
+  },
+  mounted() {
+    this.getFilms();
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/FilmList.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+    <div class="film-list" v-if="films.length">
+        <film-list-item
+            v-for="(film, index) in films"
+            :film="film"
+            :key="index"
+        >
+        </film-list-item>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: "film-list",
+    props: ["films"]
+}
+</script>
+
+<style>
+
+</style>
